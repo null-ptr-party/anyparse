f238dff0999cb7c8a220fa424d95e872104fd03d
murrayld <landondmurray@yahoo.com>
2026-01-03 21:12:33 -0800

    add prog bar and read hexchar functions

--- reader.h
+++ reader.h
@@ -2,10 +2,13 @@
 #define _READER_H
 
 #include <stdint.h>
+#define PROGBAR_LEN 40
 
 int32_t read_bytes(FILE* input_file, uint8_t outbuff[], uint32_t buffsize, uint32_t bytes_to_read);
 int32_t read_hexascii_bytes(FILE* input_file, uint8_t outbuff[], uint32_t buffsize, uint32_t bytes_to_read);
 int32_t shift_file_ptr(FILE* file, int32_t bytes_to_shift, bool read_type);
 int32_t get_file_len(FILE* file);
+int32_t prog_bar(uint32_t current, uint32_t max, uint32_t every);
+int32_t read_single_hexchar(char byte);
 
 #endif
