b3362431ca55b798fad0ad4801e28fb939996eea
murrayld <landondmurray@gmail.com>
2026-01-04 13:11:41 -0800

    update header to match function definition

--- reader.h
+++ reader.h
@@ -6,7 +6,7 @@
 
 int32_t read_bytes(FILE* input_file, uint8_t outbuff[], uint32_t buffsize, uint32_t bytes_to_read);
 int32_t read_hexascii_bytes(FILE* input_file, uint8_t outbuff[], uint32_t buffsize, uint32_t bytes_to_read);
-int32_t shift_file_ptr(FILE* file, int32_t bytes_to_shift, bool read_type);
+int32_t shift_file_ptr(FILE* file, int32_t bytes_to_shift);
 int32_t get_file_len(FILE* file);
 int32_t prog_bar(uint32_t current, uint32_t max, uint32_t every);
 int32_t read_single_hexchar(char byte);
