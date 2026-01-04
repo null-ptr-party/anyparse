d4b2771e8cca0e5d2aa0a9e4cd744653e2fad7b5
murrayld <landondmurray@yahoo.com>
2026-01-03 21:11:56 -0800

    add prog bar and read hexchar functions

--- reader.c
+++ reader.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include "reader.h"
 
-
 int32_t read_bytes(FILE* input_file, uint8_t outbuff[], uint32_t buffsize, uint32_t bytes_to_read)
 {
 	if ((input_file == NULL) || (bytes_to_read >= buffsize)) return -1; // check file pointer validity and num bytes.
@@ -56,7 +55,7 @@ int32_t shift_file_ptr(FILE* file, int32_t bytes_to_shift, bool read_type)
 {
 	if (file == NULL) return -1;
 
-	int32_t max_shift_neg = -ftell(file);
+	int32_t max_shift_neg = -ftell(file); // negative for comparison later.
 	int32_t max_shift_pos = get_file_len(file) + max_shift_neg;
 
 	if ((bytes_to_shift > max_shift_pos) || (bytes_to_shift < max_shift_neg)) return -1;
@@ -66,3 +65,65 @@ int32_t shift_file_ptr(FILE* file, int32_t bytes_to_shift, bool read_type)
 	return 0;
 }
 
+int32_t prog_bar(uint32_t current, uint32_t max, uint32_t every)
+{
+	static uint32_t call_cnt = 0;
+	if (!(call_cnt % every))
+	{
+		char str[PROGBAR_LEN] = "|_____________________________________|";
+		uint32_t prog = current * (PROGBAR_LEN - 1) / max;
+		for (uint32_t idx = 1; (idx < prog) && (idx < PROGBAR_LEN); idx++)
+		{
+			str[idx] = '=';
+		}
+		printf("%s\r", str);
+	}
+	call_cnt++;
+	return 0;
+}
+
+int32_t read_single_hexchar(char byte)
+{
+	switch(byte)
+	{
+		case '0':
+			return 0;
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;
+		case 'A':
+			return 10;
+		case 'a':
+			return 10;
+		case 'B':
+			return 11;
+		case 'b':
+			return 12;
+		case 'C':
+			return 13;
+		case 'c':
+			return 14;
+		case 'D':
+			return 15;
+		case 'd':
+			return 15;
+		default:
+			return -1; // for any unparsables.
+	}
+	return 0;
+}
