fae0c46f8a012eb34f3bdbe190aa92e80bb6c888
murrayld <landondmurray@yahoo.com>
2026-01-07 20:32:16 -0800

    move configuration functions to separate file

--- msgcfg.c
+++ msgcfg.c
@@ -1,6 +1,3 @@
-#include "parser.h"
-#include "converters.h"
-#include "reader.h"
 #include <stdint.h>
 #include <stdio.h> 
 #include <stdbool.h>
@@ -8,113 +5,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-uint64_t strip_bits(const uint8_t data_array[], const uint8_t mask_array[], uint32_t num_bytes, bool whend)
-{
-	// whend "which end" controls which end is read from.
-	uint64_t bitsum = 0;
-	uint32_t shift_counter = 0, idx = 0;
-	uint8_t mask_byte = 0, byte_stripped = 0;
-	int8_t incr = 0;
-
-	idx = (whend == 0) ? (num_bytes - 1) : 0; // set starting index based on endianness
-	incr = (whend == 0) ? -1 : 1; // increment in positive or negative direction based on selection
-	
-	for (; (idx < num_bytes) && (idx >= 0); idx += incr)
-	{	// iterate over bytes
-		mask_byte = mask_array[idx];
-		byte_stripped = mask_byte & data_array[idx]; // mask off data for this byte
-
-		if (mask_byte > 0)
-		{
-			// only iterate over bytes that have maskbits
-			for (uint32_t idy = 0; idy < 8; idy++, byte_stripped >>= 1, mask_byte >>= 1)
-			{
-				bitsum += ((uint64_t)(byte_stripped & 0x01LLU) << shift_counter); // the typecast is important here otherwise weird stuff happens.
-				shift_counter += (mask_byte & 0x01); // extra shifts after last bitsum
-			}
-		}
-	}
-	return bitsum;
-}
-
-int32_t parse_single_msg(const uint8_t bytes[], struct msg_cfg* cfg)
-{
-	if (cfg == NULL) return 1;
-
-	// top level function with be responsible for framing and ensuring no buffer overrun
-	// copy message name to output
-	uint64_t field_data = 0;
-	struct field_cfg* field_cfg_ptr = NULL;
-	struct parsed_field* pfield_ptr = NULL;
-
-	for (int field_num = 0; field_num < cfg->num_fields; field_num++)
-	{
-		pfield_ptr = pfield_by_idx(cfg, field_num);
-		field_cfg_ptr = field_cfg_by_idx(cfg, field_num);
-		if ((field_cfg_ptr == NULL) || (pfield_ptr == NULL)) return 1;
-
-		field_data = strip_bits(bytes, field_cfg_ptr->bitmask, cfg->num_bytes, cfg->whend); // extract bits
-		pfield_ptr->parsed_val = call_parser(field_data, field_cfg_ptr->converter, field_cfg_ptr->num_bits, field_cfg_ptr->dtype, field_cfg_ptr->sf); // call converter
-	}
-	return 0;
-}
-
-int32_t open_and_parse_file(const char filetoparse[], const char outputfile[], struct msg_cfg* cfg, bool readmethod)
-{
-	FILE* ftoparse = NULL;
-	FILE* fout = NULL;
-
-	if (readmethod == 1)
-	{
-		ftoparse = fopen(filetoparse, "r");
-	}
-	else
-	{
-		ftoparse = fopen(filetoparse, "rb");
-	}
-	fout = fopen(outputfile, "w");
-	
-	if (write_msg_headers(fout, cfg, 1) == 1)
-	{
-		return 1;
-	}
-	// parse but return error if issue.
-	if (parse_from_file(ftoparse, fout, cfg, readmethod) == 1)
-	{
-		return 1;
-	}
-
-	fclose(ftoparse);
-	fclose(fout);
-	return 0;
-}
-
-int32_t parse_from_file(FILE* ftoparse, FILE* fparsed, struct msg_cfg* cfg, bool read_type)
-{
-	if ((ftoparse == NULL) || (fparsed == NULL)) return 1;
-
-	int32_t file_len = get_file_len(ftoparse);
-	uint8_t bindata_buff[BINDATABUFF_SIZE] = { 0 };
-	uint8_t bytes_per_message = cfg->num_bytes;
-	while (ftell(ftoparse) <= file_len) // parse up to EOF
-	{
-		prog_bar((uint32_t)ftell(ftoparse), (uint32_t)file_len, 100000);
-		if (read_type == 1)
-		{
-			if (read_hexascii_bytes(ftoparse, bindata_buff, BINDATABUFF_SIZE, bytes_per_message) != bytes_per_message) break;
-		}
-		else
-		{
-			if (read_bytes(ftoparse, bindata_buff, BINDATABUFF_SIZE, bytes_per_message) != bytes_per_message) break;
-		}
-		parse_single_msg(bindata_buff, cfg);
-		parsed_msg_to_file(fparsed, cfg, 1);
-	}
-
-	return 0;
-}
 /*=============================== Setup Functions ======================================================*/
-
 int32_t init_msgcfg(struct msg_cfg* cfg, char fieldname[], uint8_t num_bytes, bool whend)
 {
 	// Note that number of fields is incremented as fields are added.
@@ -130,8 +21,13 @@ int32_t init_msgcfg(struct msg_cfg* cfg, char fieldname[], uint8_t num_bytes, bo
 
 int32_t update_msgcfg(struct msg_cfg* cfg, char fieldname[], uint8_t num_bytes, bool whend)
 {
+	// Note that number of fields is incremented as fields are added.
 	if (cfg == NULL) return 1;
 	strncpy(cfg->messagename, fieldname, MAX_FIELDNAME_LEN);
+	cfg->num_bytes = num_bytes;
+	cfg->whend = whend;
+
+	return 0;
 }
 // add field to message in position 0.
 int32_t add_field_to_msgcfg(struct msg_cfg* cfg, const uint8_t bitmask[MAX_BITMASK_LEN_BYTES], const char fieldname[], uint8_t converter_select, uint8_t dtype, double sf)
@@ -142,7 +38,7 @@ int32_t add_field_to_msgcfg(struct msg_cfg* cfg, const uint8_t bitmask[MAX_BITMA
 		printf("Too many fields\n");
 		return 1;
 	}
-	
+
 	// first in linked list is the message
 	struct field_cfg* new_field = (struct field_cfg*)malloc(sizeof(struct field_cfg)); // allocation memory for new struct.
 	struct parsed_field* new_pfield = (struct parsed_field*)malloc(sizeof(struct parsed_field)); // 
@@ -155,7 +51,7 @@ int32_t add_field_to_msgcfg(struct msg_cfg* cfg, const uint8_t bitmask[MAX_BITMA
 	}
 
 	if (cfg->num_fields == 0)
-	{	
+	{
 		cfg->first_field = new_field;
 		cfg->first_pfield = new_pfield;
 	}
@@ -266,10 +162,10 @@ int32_t append_field(struct msg_cfg* cfg,
 	return 0;
 }
 
-int32_t add_field_at_idx(struct msg_cfg* cfg, uint32_t field_idx, 
-						const uint8_t bitmask[MAX_BITMASK_LEN_BYTES], const char fieldname[],
-						uint8_t converter_select, uint8_t dtype,
-						double sf)
+int32_t add_field_at_idx(struct msg_cfg* cfg, uint32_t field_idx,
+	const uint8_t bitmask[MAX_BITMASK_LEN_BYTES], const char fieldname[],
+	uint8_t converter_select, uint8_t dtype,
+	double sf)
 {
 	// Highest index a field can be added is n = num_fields-1
 	// anything higher should return error.
@@ -332,7 +228,7 @@ int32_t add_field_at_idx(struct msg_cfg* cfg, uint32_t field_idx,
 	new_field->num_bits = bits_in_bitmask(new_field->bitmask, cfg->num_bytes);
 
 	cfg->num_fields++;
-	
+
 	return 0;
 }
 
@@ -365,7 +261,7 @@ int32_t rm_field_by_idx(struct msg_cfg* cfg, uint32_t field_idx)
 			cfg->first_field = field_cfg_ptr->next_field;
 			cfg->first_pfield = pfield_ptr->next_field;
 		}
-		else if(field_idx > 0)
+		else if (field_idx > 0)
 		{	// if fields greater than 0, we need to re-link
 			// previous field with next.
 			prev_field_cfg_ptr->next_field = field_cfg_ptr->next_field;
@@ -468,92 +364,9 @@ int32_t rm_all_msg_fields(struct msg_cfg* cfg)
 	return 0;
 }
 
-
-/*=============================== Output Writing ===========================================================*/
-
-int32_t write_msg_headers(FILE* output_file, struct msg_cfg* cfg, uint8_t newlineforlast)
-{
-	if ((output_file == NULL) || (cfg == NULL)) return 1;
-	struct field_cfg* field_cfg = NULL;
-
-	for (uint32_t idx = 0; idx < cfg->num_fields; idx++)
-	{
-		field_cfg = field_cfg_by_idx(cfg, idx);
-		if (field_cfg == NULL) return 1;
-
-		fprintf(output_file, "%s.%s", cfg->messagename, field_cfg->fieldname);
-		if ((idx < cfg->num_fields - 1))
-		{
-			fprintf(output_file, ", ");
-		}
-	}
-
-	if (newlineforlast)
-	{
-		fprintf(output_file, "\n");
-	}
-	else
-	{
-		fprintf(output_file, ", ");
-	}
-
-	return 0;
-}
-
-int32_t parsed_msg_to_file(FILE* output_file, struct msg_cfg* cfg, uint8_t newlineforlast)
-{
-	if ((output_file == NULL) || (cfg == NULL)) return 1;
-
-	uint8_t dtype = 0;
-	struct parsed_field* pfield_ptr = cfg->first_pfield;
-	if (pfield_ptr == NULL) return 1;
-
-	for (uint32_t idx = 0; (pfield_ptr != NULL) && (idx < cfg->num_fields); idx++, pfield_ptr = pfield_ptr->next_field)
-	{
-		dtype = pfield_ptr->dtype;
-
-		switch (dtype)
-		{
-			case DTYPE_OUT_INT:
-				fprintf(output_file, "%lld", pfield_ptr->parsed_val.int_result);
-				break;
-
-			case DTYPE_OUT_FLOAT:
-				fprintf(output_file, "%lf", pfield_ptr->parsed_val.float_result);
-				break;
-
-			case DTYPE_OUT_CHAR:
-				fprintf(output_file, "%c", pfield_ptr->parsed_val.char_result[0]);
-				break;
-
-			case DTYPE_OUT_UINT:
-				fprintf(output_file, "%llu", pfield_ptr->parsed_val.uint_result);
-				break;
-
-			default:
-				;
-		}
-
-		if (idx < (cfg->num_fields - 1))
-		{
-			fprintf(output_file, ", "); // comma only between fields.
-		}
-	}
-
-	if (newlineforlast)
-	{
-		fprintf(output_file, "\n");
-	}
-	else
-	{
-		fprintf(output_file, ", ");
-	}
-
-	return 0;
-}
 /*=============================== Utilities ===========================================================*/
 uint32_t bits_in_bitmask(const uint8_t bitmask[], uint32_t num_bytes)
-{	
+{
 	uint32_t bitct = 0, byte = 0;
 
 	for (uint32_t byte_idx = 0; byte_idx < num_bytes; byte_idx++)
@@ -575,7 +388,7 @@ int32_t bitmask_from_cfgstr(char cfg_str[], uint8_t bitmask[MAX_BITMASK_LEN_BYTE
 	uint32_t len = strlen(cfg_str);
 	if (strlen(cfg_str) >= MAX_BITMASK_STR_LEN) return 1;
 
-	uint32_t parsed_num_buff[3*MAX_BITMASK_LEN_BYTES] = { 0 };
+	uint32_t parsed_num_buff[3 * MAX_BITMASK_LEN_BYTES] = { 0 };
 	uint32_t buff_idx = 0;
 	char* const firstchar_ptr = cfg_str; // get pointer to first char of string.
 	char* tknptr = NULL;
@@ -588,7 +401,7 @@ int32_t bitmask_from_cfgstr(char cfg_str[], uint8_t bitmask[MAX_BITMASK_LEN_BYTE
 			sscanf(tknptr, "%u", &parsed_num_buff[buff_idx]);
 			buff_idx++;
 		}
-	tknptr = strtok(NULL, "[,]"); // get next delimiter.
+		tknptr = strtok(NULL, "[,]"); // get next delimiter.
 	}
 
 	if (((buff_idx % 3) != 0) || (buff_idx == 0)) return 1; // need sets of 3 to build bitmask
@@ -600,8 +413,8 @@ int32_t bitmask_from_cfgstr(char cfg_str[], uint8_t bitmask[MAX_BITMASK_LEN_BYTE
 		byte_idx = parsed_num_buff[idx];
 		if (byte_idx >= MAX_BITMASK_LEN_BYTES) return 1;
 
-		strtbit = parsed_num_buff[idx+num_bytes];
-		stpbit = parsed_num_buff[idx+2*num_bytes];
+		strtbit = parsed_num_buff[idx + num_bytes];
+		stpbit = parsed_num_buff[idx + 2 * num_bytes];
 		if ((strtbit > 7) || (stpbit > 7)) return 1; // cant exceed num bits in byte
 
 		if (strtbit == 0)
---
+++ msgcfg.h
@@ -0,0 +1,75 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// macro defs
+#define BIG_ENDIAN 0
+#define LITTLE_ENDIAN 1
+#define MAX_NUM_FIELDS 10
+#define MAX_BITMASK_LEN_BYTES 20
+#define MAX_FIELDNAME_LEN 30
+#define RESULT_IS_INT 0
+#define RESULT_IS_FLOAT 1
+#define MSGBUFF_SIZE 1024
+#define BINDATABUFF_SIZE 1024
+#define MAX_BITMASK_STR_LEN 500
+
+// struct defs
+struct msg_cfg {
+	char messagename[MAX_FIELDNAME_LEN];
+	uint8_t num_bytes;
+	uint8_t num_fields;
+	bool whend; // Endianness, which end.
+	struct field_cfg* first_field; // used for storing field configs.
+	struct parsed_field* first_pfield; // used for storing parsed fields.
+};
+
+// field cfg struct
+struct field_cfg {
+	char fieldname[MAX_FIELDNAME_LEN];
+	uint8_t bitmask[MAX_BITMASK_LEN_BYTES]; // bitmask for removing bits
+	uint8_t num_bits;
+	uint8_t converter;
+	uint8_t dtype; // result will be typcast if not
+	double sf;
+	struct field_cfg* next_field;
+};
+
+// parsed field struct
+struct parsed_field {
+	char fieldname[MAX_FIELDNAME_LEN];
+	uint8_t dtype;
+	union parsed_result parsed_val;
+	struct parsed_field* next_field;
+};
+
+/*=============================== Setup/Config Functions ======================================================*/
+// initialize message config
+int32_t init_msgcfg(struct msg_cfg* cfg, char fieldname[], uint8_t num_bytes, bool whend);
+// update message configuration
+int32_t update_msgcfg(struct msg_cfg* cfg, char fieldname[], uint8_t num_bytes, bool whend)
+// add field to message config
+int32_t add_field_to_msgcfg(struct msg_cfg* cfg, const uint8_t bitmask[MAX_BITMASK_LEN_BYTES], const char fieldname[], uint8_t converter_select, uint8_t dtype, double sf);
+// get field config by index.
+struct field_cfg* field_cfg_by_idx(struct msg_cfg* cfg, uint32_t field_idx);
+// append field to end of config.
+int32_t append_field(struct msg_cfg* cfg,
+	const uint8_t bitmask[], const char fieldname[],
+	uint8_t converter_select, uint8_t dtype,
+	double sf);
+// add field at index
+int32_t add_field_at_idx(struct msg_cfg* cfg, uint32_t field_idx,
+	const uint8_t bitmask[MAX_BITMASK_LEN_BYTES], const char fieldname[],
+	uint8_t converter_select, uint8_t dtype,
+	double sf);
+// remove specific field by index.
+int32_t rm_field_by_idx(struct msg_cfg* cfg, uint32_t field_idx);
+// get parsed field by index
+struct parsed_field* pfield_by_idx(struct msg_cfg* cfg, uint32_t field_idx);
+// get field config by name
+struct field_cfg* get_field_cfg_by_name(struct msg_cfg* cfg, const char fieldname[]);
+// get parsed field by name
+struct parsed_field* get_pfield_by_name(struct msg_cfg* cfg, const char fieldname[]);
+// Remove the first field of message config.
+int32_t rm_first_field(struct msg_cfg* cfg);
+// Remove all fields from message config.
+int32_t rm_all_msg_fields(struct msg_cfg* cfg);
